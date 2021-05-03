604d7799785999aa005c9f1f246091f361857935
Vamshi Krishna <bommvams72@gmail.com>
2021-05-04 00:59:56 +0530

    Add filter for 18+ age group

--- vac_locations/views.py
+++ vac_locations/views.py
@@ -11,7 +11,9 @@ def searchByDistrict(request):
     if response.status_code == 200:
         data = response.json()
         # print(data)
-        return render(request, "vac_locations_searchByDistrict.html", context=data)
+        today=datetime.date.today()
+        data['today'] = today.strftime("%Y-%m-%d")
+        return render(request, "vac_locations_searchByDistrict.html", data)
     else:
         # TODO: error handling
         return error()
@@ -28,17 +30,24 @@ def searchByDistrictResults(request):
         district_id  = request.POST['district_id']
         yyyy,mm,dd=request.POST['date'].split('-')
         params = {'district_id': district_id, 'date': '-'.join([dd,mm,yyyy])}
+        dataFilter=dict()
+        if "Above18Only" in request.POST:
+            dataFilter['Above18Only']= request.POST['Above18Only']
         if "getFor7Days" in request.POST and request.POST['getFor7Days']=="yes":
             inp_date = datetime.datetime(int(yyyy), int(mm), int(dd))
             datesofNext7days = [(inp_date + datetime.timedelta(days=count)).strftime("%d-%m-%Y") for count in range(0,7)]
             response = requests.get(COWIN_BASE_URL+"/v2/appointment/sessions/public/calendarByDistrict",params)
             data=response.json()
             data['datesofNext7days']=datesofNext7days
-            print(datesofNext7days)
+            for key, value in dataFilter.items():
+                data[key] = value
+            print(data)
             return render(request,"vac_locations_searchResultsFor7days.html",data)
         else:
             response = requests.get(COWIN_BASE_URL+"/v2/appointment/sessions/public/findByDistrict",params)
             data=response.json()
+            for key, value in dataFilter.items():
+                data[key] = value
             print(data)
             return render(request,"vac_locations_searchResults.html",data)
     else:
@@ -49,24 +58,32 @@ def searchByPinCodeResults(request):
     if "pincode" in request.POST and "date" in request.POST:
         pincode = request.POST['pincode']
         yyyy,mm,dd=request.POST['date'].split('-')
+        dataFilter=dict()
         params = {'pincode': pincode, 'date': '-'.join([dd,mm,yyyy])}
+        if "Above18Only" in request.POST:
+            dataFilter['Above18Only']= request.POST['Above18Only']
         if "getFor7Days" in request.POST and request.POST['getFor7Days']=="yes":
             inp_date = datetime.datetime(int(yyyy), int(mm), int(dd))
             datesofNext7days = [(inp_date + datetime.timedelta(days=count)).strftime("%d-%m-%Y") for count in range(0,7)]
             response = requests.get(COWIN_BASE_URL+"/v2/appointment/sessions/public/calendarByPin",params)
             data=response.json()
             data['datesofNext7days']=datesofNext7days
+            for key, value in dataFilter.items():
+                data[key] = value
             print(data)
+            process_data(data,data["Above18Only"]=="yes")
             return render(request,"vac_locations_searchResultsFor7days.html",data)
         else:
             response = requests.get(COWIN_BASE_URL+"/v2/appointment/sessions/public/findByPin",params=params)
             data=response.json()
+            for key, value in dataFilter.items():
+                data[key] = value
             print(data)
             return render(request,"vac_locations_searchResults.html",data)
     else:
         # TODO error hanlding
         return render(request,"vac_locations_searchResults.html")
-
+ 
 # def notify():
 #     # send email if found
 
@@ -90,6 +107,19 @@ def load_districts(request):
             return render(request,"error.html")
     return render(request, "vac_locations_districts.html", context=data)
 
+def process_data(data,above18Only=False):
+    # data=json.loads(response)
+    count=0
+    for center in data['centers']:
+        for session in center['sessions']:
+            print(session['min_age_limit'])
+            if(above18Only and session['min_age_limit']<=18):
+                count+=session['available_capacity']
+            elif not above18Only:
+                count+=session['available_capacity']
+    print(count)
 
 def searchByPinCode(request):
-    return render(request, "vac_locations_searchByPinCode.html")
+    today=datetime.date.today()
+    data={'today': today.strftime("%Y-%m-%d")}
+    return render(request, "vac_locations_searchByPinCode.html", data)
